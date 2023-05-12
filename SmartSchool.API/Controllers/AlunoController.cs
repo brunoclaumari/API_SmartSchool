e044b44d48d6e70bf7ac61354a0267a03b9ba813
brunoclaumari <brunoclaumari@gmail.com>
2023-05-12 02:03:38 -0300

    Add migrations e ProdutoController

--- SmartSchool.API/Controllers/AlunoController.cs
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using SmartSchool.API.Data;
 using SmartSchool.API.Models;
 using System;
 using System.Collections.Generic;
@@ -14,45 +16,19 @@ namespace SmartSchool.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class AlunoController : ControllerBase
-    {
-        private List<Aluno> ListaAlunos;
+    {        
 
         private const string Alunos = "Alunos";
+        private readonly SmartContext _smartContext;
 
-        public AlunoController()
+        public AlunoController(SmartContext smartContext)
         {
-            ListaAlunos = new List<Aluno>();
-            //ManipulaDadosSessao();
-        }
-         
-        private void ManipulaDadosSessao()
-        {
-            string dados = string.Empty;
-            try
-            {
-                dados = HttpContext.Session.GetString(Alunos);
-                if (!string.IsNullOrEmpty(dados))
-                    ListaAlunos = JsonConvert.DeserializeObject<List<Aluno>>(dados);
-                else
-                    InicializaLista(dados);
-            }
-            catch (Exception e)
-            {
-                InicializaLista(dados);
-            }
-        }
-
-        private void InicializaLista(string dados)
-        {            
-            SeedTemporario seed = new SeedTemporario();
-            this.ListaAlunos = seed.ListaAlunos;
-            dados = JsonConvert.SerializeObject(ListaAlunos);
-            HttpContext.Session.SetString(Alunos, dados);
+            _smartContext = smartContext;
         }
 
         private void SalvaDadosSessao()
         {
-            HttpContext.Session.SetString(Alunos, JsonConvert.SerializeObject(ListaAlunos));
+            //HttpContext.Session.SetString(Alunos, JsonConvert.SerializeObject(ListaAlunos));
         }
 
 
@@ -60,16 +36,17 @@ namespace SmartSchool.API.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            ManipulaDadosSessao();
-            return Ok(ListaAlunos);
+            var lista = _smartContext.Alunos;
+
+            return Ok(lista);
         }
 
         // GET api/<AlunoController>/5
         [HttpGet("byId/{id}")]
         public IActionResult GetById(int id)
         {
-            ManipulaDadosSessao();
-            Aluno aluno = ListaAlunos.FirstOrDefault(x => x.Id == id);
+            
+            Aluno aluno = _smartContext.Alunos.FirstOrDefault(x => x.Id == id);
 
             if (aluno is null) return BadRequest($"Aluno id = {id} não encontrado!!");
 
@@ -79,8 +56,7 @@ namespace SmartSchool.API.Controllers
         [HttpGet("byName")]
         public IActionResult GetByName(string nome, string sobrenome)
         {
-            ManipulaDadosSessao();
-            Aluno aluno = ListaAlunos.FirstOrDefault(x => x.Nome.Contains(nome) && x.Sobrenome.Contains(sobrenome));
+            Aluno aluno =  _smartContext.Alunos.FirstOrDefault(x => x.Nome.Contains(nome) && x.Sobrenome.Contains(sobrenome));
 
             if (aluno is null) return BadRequest($"Aluno nome completo {nome} {sobrenome} não encontrado!!");
 
@@ -91,35 +67,36 @@ namespace SmartSchool.API.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Aluno value)
         {
-            ManipulaDadosSessao();
-            int ultimoId = ListaAlunos.LastOrDefault().Id;
-            value.Id = ultimoId + 1;
-            ListaAlunos.Add(value);
-            SalvaDadosSessao();
+            _smartContext.Alunos.Add(value);
+            _smartContext.SaveChanges();
 
-            return Ok(value);
+            return CreatedAtAction("Post", value);
         }
 
         // PUT api/<AlunoController>/5
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] Aluno value)
         {
-            ManipulaDadosSessao();
-            Aluno aluno = ListaAlunos.FirstOrDefault(x => x.Id == id);
-            int indice = ListaAlunos.IndexOf(aluno);
+            Aluno aluno = _smartContext.Alunos.AsNoTracking().FirstOrDefault(x => x.Id == id);
+
             if (aluno is null) return BadRequest($"Aluno id = {id} não encontrado!!");
             value.Id = aluno.Id;
-            ListaAlunos[indice] = value;
-            SalvaDadosSessao();
+            _smartContext.Alunos.Update(value);
+            _smartContext.SaveChanges();
 
             return Ok(value);
         }
 
-        // PUT api/<AlunoController>/5
+        // PATCH api/<AlunoController>/5
         [HttpPatch("{id}")]
         public IActionResult Patch(int id, [FromBody] Aluno value)
         {
-            ManipulaDadosSessao();
+            Aluno aluno = _smartContext.Alunos.AsNoTracking().FirstOrDefault(x => x.Id == id);
+
+            if (aluno is null) return BadRequest($"Aluno id = {id} não encontrado!!");
+            value.Id = aluno.Id;
+            _smartContext.Alunos.Update(value);
+            _smartContext.SaveChanges();
 
             return Ok(value);
         }
@@ -128,11 +105,11 @@ namespace SmartSchool.API.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            ManipulaDadosSessao();
-            Aluno aluno = ListaAlunos.FirstOrDefault(x => x.Id == id);
+            Aluno aluno = _smartContext.Alunos.FirstOrDefault(x => x.Id == id);
             if (aluno is null) return BadRequest($"Aluno id = {id} não encontrado!!");
-            ListaAlunos.Remove(aluno);
-            SalvaDadosSessao();
+
+            _smartContext.Alunos.Remove(aluno);
+            _smartContext.SaveChanges();
 
             return NoContent();
         }
---
+++ SmartSchool.API/Controllers/ProfessorController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.API.Data;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        private readonly SmartContext _smartContext;
+
+        public ProfessorController(SmartContext smartContext)
+        {
+            _smartContext = smartContext;
+        }
+
+
+
+
+        // GET: api/<ProfessorController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var prof = _smartContext.Professores;
+
+            return Ok(prof);
+        }
+
+        // GET api/<ProfessorController>/5
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            Professor prof = _smartContext.Professores.FirstOrDefault(x => x.Id == id);
+
+            if (prof is null) return BadRequest($"Professor id = {id} não encontrado!!");
+
+            return Ok(prof);
+        }
+
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome)
+        {
+            Professor prof = _smartContext.Professores.FirstOrDefault(x => x.Nome.Contains(nome));
+
+            if (prof is null) return BadRequest($"Professor nome: {nome} não encontrado!!");
+
+            return Ok(prof);
+        }
+
+        // POST api/<ProfessorController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Professor value)
+        {
+            _smartContext.Professores.Add(value);
+            _smartContext.SaveChanges();
+
+            return CreatedAtAction("Post", value);
+        }
+
+        // PUT api/<ProfessorController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Professor value)
+        {
+            Professor prof = _smartContext.Professores.AsNoTracking().FirstOrDefault(x => x.Id == id);
+
+            if (prof is null) return BadRequest($"Professor id = {id} não encontrado!!");
+            value.Id = prof.Id;
+            _smartContext.Professores.Update(value);
+            _smartContext.SaveChanges();
+
+            return Ok(value);
+        }
+
+        // PATCH api/<AlunoController>/5
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] Professor value)
+        {
+            Professor prof = _smartContext.Professores.AsNoTracking().FirstOrDefault(x => x.Id == id);
+
+            if (prof is null) return BadRequest($"Professor id = {id} não encontrado!!");
+            value.Id = prof.Id;
+            _smartContext.Professores.Update(value);
+            _smartContext.SaveChanges();
+
+            return Ok(value);
+        }
+
+        // DELETE api/<ProfessorController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Professor prof = _smartContext.Professores.FirstOrDefault(x => x.Id == id);
+            if (prof is null) return BadRequest($"Professor id = {id} não encontrado!!");
+
+            _smartContext.Professores.Remove(prof);
+            _smartContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
